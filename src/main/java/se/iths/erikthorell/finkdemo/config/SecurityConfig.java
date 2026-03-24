cbd1633a9fc0dd7f9f16b9e603e0dbe5af58ba6d
eritho91 <erik.thorell@iths.se>
2026-03-24 14:28:27 +0100

    nya html-sidor

--- src/main/java/se/iths/erikthorell/finkdemo/config/SecurityConfig.java
+++ src/main/java/se/iths/erikthorell/finkdemo/config/SecurityConfig.java
@@ -3,30 +3,30 @@ package se.iths.erikthorell.finkdemo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/register", "/css/**").permitAll()
+                        .requestMatchers("/register", "/css/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                        .loginPage("/")
-                        .defaultSuccessUrl("/posts", true)
+                        .loginPage("/login")       // sidan med ditt formulär
+                        .defaultSuccessUrl("/", true)
                         .permitAll()
                 )
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                        .permitAll()
+                .logout(LogoutConfigurer::permitAll
                 );
-
         return http.build();
     }
 
