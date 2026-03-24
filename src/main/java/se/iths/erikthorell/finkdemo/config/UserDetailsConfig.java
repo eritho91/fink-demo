a9288aef210c0915d7f0a46f2a5712ccb42069f3
eritho91 <erik.thorell@iths.se>
2026-03-24 14:55:23 +0100

    nya html-sidor

---
+++ src/main/java/se/iths/erikthorell/finkdemo/config/UserDetailsConfig.java
@@ -0,0 +1,28 @@
+package se.iths.erikthorell.finkdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import se.iths.erikthorell.finkdemo.repository.UserRepository;
+
+@Configuration
+public class UserDetailsConfig {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getUsername())
+                        .password(user.getPassword())
+                        .roles("USER") // standardroll
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("Användare ej hittad: " + username));
+    }
+}
